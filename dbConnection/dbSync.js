6881517f88c1e913c4ed2c6d87e6064fe766f085
Satish Singh <i.sksingh113@gmail.com>
2025-09-04 19:56:21 +0530

    some

--- dbConnection/dbSync.js
+++ dbConnection/dbSync.js
@@ -12,17 +12,17 @@ const RequestHistory = require("../model/historyModel/requestHistory");
 const initDB = (callback) => {
   sequelize.authenticate()
     .then(() => {
-      console.log(' Database connected');
+      console.log('Connected to Supabase PostgreSQL');
       require('../model/associationModel/association');
       return sequelize.sync(); // Creates tables if not exist {alter:true}
     })
     .then(() => {
-      console.log(' All models synced');
+      console.log('All models synced');
       callback(); 
     })
     .catch((error) => {
-      console.error(' Error connecting to the database:', error);
-      process.exit(1); 
+      console.error('Error connecting to the database:', error);
+      process.exit(1);
     });
 };
 module.exports = initDB;
