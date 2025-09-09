65395858b4d9a7f997ffd8baff25ba9709eca78b
Satish Singh <i.sksingh113@gmail.com>
2025-09-09 09:55:35 +0530

    fixes

--- authService/clearTokenCookie.js
+++ authService/clearTokenCookie.js
@@ -2,7 +2,7 @@ const { serialize } = require("cookie");
 const clearTokenCookie = (res) => {
   res.setHeader("Set-Cookie", [
     serialize("token", "", {
-       domain:".pixbit.me",
+       domain:".matchandfix.com",
       httpOnly: true,
       secure: true,
       sameSite: "none",
@@ -10,7 +10,7 @@ const clearTokenCookie = (res) => {
       expires: new Date(0),
     }),
     serialize("token_middleware", "", {
-       domain:".pixbit.me",
+       domain:".matchandfix.com",
       httpOnly: false,
       secure: true,
       sameSite: "none",
