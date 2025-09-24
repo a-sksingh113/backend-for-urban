39353f2e835b6378b6f6aca9a6d7401c3676ca65
Satish Singh <i.sksingh113@gmail.com>
2025-09-25 01:46:49 +0530

    some

--- routes/requestHistory/requestRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { handleGetUserHistory, handleGetUserHistoryById } = require("../../controller/historyController/requestHistory");
-const router = express.Router();
-
-router.get('/history',handleGetUserHistory);
-router.get("/history/:historyId",  handleGetUserHistoryById);
-
-module.exports = router;
