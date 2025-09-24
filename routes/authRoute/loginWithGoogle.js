33c333ebd9e93d5e753025d22a169198ea0184e9
Satish Singh <i.sksingh113@gmail.com>
2025-09-25 02:00:52 +0530

    somr

--- routes/authRoute/loginWithGoogle.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { redirectToGoogle, googleCallback } = require('../../controller/userController/googleLoginController');
-
-const router = express.Router();
-
-router.get('/google', redirectToGoogle);
-router.get('/google/callback', googleCallback);
-
-module.exports = router;
